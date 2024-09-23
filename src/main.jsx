758d198f46b9f59d503c677a17e68792e63cb19f
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-09-23 14:26:34 +0700

    add-props-type

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,27 @@
-import { StrictMode } from "react"
+import { StrictMode, useState } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
 import StarRating from "./StarRating"
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0)
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movies was rated {movieRating} stars</p>
+    </div>
+  )
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={1} />
+    <Test />
   </StrictMode>
 )
